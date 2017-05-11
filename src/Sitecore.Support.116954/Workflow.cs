2e597d2abfbaabdf1314389f06a18a5e07d28bc8
Alex Yaremenko <ay@sitecore.net>
2017-05-11 13:20:05 +0300

    Fix

---
+++ src/Sitecore.Support.116954/Workflow.cs
@@ -0,0 +1,29 @@
+﻿    namespace Sitecore.Support.Workflows.Simple
+    {
+        using Sitecore.Configuration;
+        using Sitecore.Data;
+        using Sitecore.Data.Items;
+        using Sitecore.Workflows.Simple;
+        using System;
+        using System.Runtime.Serialization;
+
+        public class Workflow : Sitecore.Workflows.Simple.Workflow
+        {
+            public Workflow(SerializationInfo info, StreamingContext context) : base(info, context)
+            {
+            }
+
+            public Workflow(string workflowID, Sitecore.Support.Workflows.Simple.WorkflowProvider owner) : base(workflowID, owner)
+            {
+            }
+
+            public override bool IsApproved(Item item, Database targetDatabase)
+            {
+                if (Settings.ItemCloning.InheritWorkflowData && item.IsClone)
+                {
+                    return base.IsApproved(item.Source, targetDatabase);
+                }
+                return base.IsApproved(item, targetDatabase);
+            }
+        }
+    }
---
+++ src/Sitecore.Support.116954/WorkflowProvider.cs
@@ -0,0 +1,45 @@
+﻿namespace Sitecore.Support.Workflows.Simple
+{
+    using Sitecore.Configuration;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Workflows;
+
+    public class WorkflowProvider : Sitecore.Workflows.Simple.WorkflowProvider
+    {
+        public WorkflowProvider(string databaseName, HistoryStore historyStore) : base(databaseName, historyStore)
+        {
+        }
+
+        private static string GetInheritedWorkflowID(Item item)
+        {
+            string workflowID = string.Empty;
+            Assert.ArgumentNotNull(item, "item");
+            if (item.Database.DataManager.DataSource.GetWorkflowInfo(item.ID, item.Language, item.Version) == null)
+            {
+                string str2 = item.Fields[FieldIDs.Workflow].GetValue(false, false, false, true);
+                string str3 = item.Fields[FieldIDs.WorkflowState].GetValue(false, false, false, true);
+                if (!(string.IsNullOrEmpty(str2) || string.IsNullOrEmpty(str3)))
+                {
+                    WorkflowInfo info = new WorkflowInfo(str2, str3);
+                    workflowID = info.WorkflowID;
+                }
+            }
+            return workflowID;
+        }
+
+        public override IWorkflow GetWorkflow(Item item)
+        {
+            Assert.ArgumentNotNull(item, "item");
+            string workflowId = (Settings.ItemCloning.InheritWorkflowData && item.IsClone) ? GetInheritedWorkflowID(item) : Sitecore.Workflows.Simple.WorkflowProvider.GetWorkflowID(item);
+            if (workflowId.Length > 0)
+            {
+                return this.InstantiateWorkflow(workflowId, this);
+            }
+            return null;
+        }
+
+        protected override IWorkflow InstantiateWorkflow(string workflowId, Sitecore.Workflows.Simple.WorkflowProvider owner) =>
+            new Sitecore.Support.Workflows.Simple.Workflow(workflowId, this);
+    }
+}
